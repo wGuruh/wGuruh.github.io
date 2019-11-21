61e202ff3c84c17890ff5bfb4eb12c6491482c05
wGuruh <wicaksana025@gmail.com>
2019-11-21 12:36:36 +0700

    UGD13_C_9236

---
+++ precache-manifest.e7b4e3b4e7a7c57d324b6451bd059322.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74e4f0744708ea5a7087",
+    "url": "/css/chunk-vendors.48fa860f.css"
+  },
+  {
+    "revision": "d7e7e1b8ad9a0689458b2e5ba4c5f254",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c60baa786b5c8fc4f261",
+    "url": "/js/app.01303d3c.js"
+  },
+  {
+    "revision": "74e4f0744708ea5a7087",
+    "url": "/js/chunk-vendors.9dbb3ed4.js"
+  },
+  {
+    "revision": "d3c7f7a24f2e88f09267a93c8d5a9d3e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
